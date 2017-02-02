a5745d50ff92054b69ac20e07a273849959e7bed
Rashid Kazmi <rashidkazmi14@gmail.com>
2017-02-02 19:01:10 +0100

    Add files via upload

---
+++ Lesson_1_Problems/number_hours_week.py
@@ -0,0 +1,7 @@
+# Write Python code that prints out the number of hours in 7 weeks.
+hrs_one_day = 24
+days_in_week = 7
+hours_in_week = (hrs_one_day * days_in_week*7)
+print (hours_in_week)
+
+print (24*7*7)
---
+++ Lesson_1_Problems/udacious.py
@@ -0,0 +1,31 @@
+# Given the variables s and t defined as:
+s = 'udacity'
+t = 'bodacious'
+# write Python code that prints out udacious
+# without using any quote characters in
+# your code.
+ #print (s[0:1] + t[2:])
+#print (s[0:1]) +: (t[2:])
+
+print (s[0:1] + t[2:])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# s1 = s[0]
+#
+# s2 = t[2:]
+#
+# print s1 + s2
+# #print s1
+# #print s2
